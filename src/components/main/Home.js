01fff12fe7431712d32af8c948935ceaac493297
Holutunde <Holutunde99@gmail.com>
2022-02-24 13:37:05 +0100

    Modified button

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -5,6 +5,7 @@ import Product2 from './image-product-2.jpg'
 import Product3 from './image-product-3.jpg'
 import Product4 from './image-product-4.jpg'
 const Home = () => {
+  const { itemNumber, setItemNumber } = useState(0)
   const [cartNo, setCartNo] = useState(0)
   return (
     <div className="hm-container">
@@ -18,16 +19,18 @@ const Home = () => {
         </div>
       </div>
       <div className="hm-right">
-        <p className="txt1">SNEAKER COMPANY</p>
-        <h2 className="txt2">Fall Limited Edition Sneakers</h2>
+        <h3 className="txt1">SNEAKER COMPANY</h3>
+        <h3 className="txt2">Fall Limited Edition Sneakers</h3>
         <p className="txt3">
           These low-profile snaeakers are your casual wear companion. Featuring
           a durable rubber outisde sole, they'll withstand anything the weather
           can offer{' '}
         </p>
         <div className="hm-right-2">
-          <p className="amount">$125.00</p>
-          <p className="dis">50%</p>
+          <h3 className="amount">$125.00</h3>
+          <div className="dis-cont">
+            <h4 className="dis">50%</h4>
+          </div>
         </div>
         <div className="hm-right-3">
           <div className="btn-cont">
@@ -47,6 +50,13 @@ const Home = () => {
             <button>{cartNo}</button>
             <button onClick={() => setCartNo(cartNo + 1)}>+</button>
           </div>
+          <button
+            className="add-cart"
+            onClick={() => setItemNumber(itemNumber + cartNo)}
+          >
+            {' '}
+            Add to cart{' '}
+          </button>
         </div>
       </div>
     </div>
